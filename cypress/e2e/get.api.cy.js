acfb5563e702c69da26a3f6c57ff51b25fd21055
Edson José dos Santos <edsonj82@outlook.com>
2024-03-14 17:28:11 -0300

    Validating call return for GET (wip)

--- cypress/e2e/get.api.cy.js
+++ cypress/e2e/get.api.cy.js
@@ -2,10 +2,25 @@
 
 describe('Search devices', () => {
     it('search aspecific device', () => {
+
+        const device_id = '7'
         cy.request({
             method: 'GET',
-            url:'https://api.restful-api.dev/objects/7',
+            url: `https://api.restful-api.dev/objects/${device_id}`,
             failOnStatusCode: false
-        })
+        }).as('getDeviceResult')
+        cy.get('@getDeviceResult')
+            .then((response) => {
+                // console.log('STATUS CODE', response.status)
+                expect(response.body).not.empty
+                expect(response.status).equal(200)
+                expect(response.body.id).equal(String(device_id))
+                expect(response.body.name).equal('Apple MacBook Pro 16')
+                expect(response.body.data).not.empty
+                expect(response.body.data.year).not.string
+                expect(response.body.data.price).equal(1849.99)
+                expect(response.body.data['CPU model']).not.empty
+                expect(response.body.data['Hard disk size']).not.empty
+            })
     });
 })
